4d40888891141b554556415a28c2790cb8f560c9
Naman pokhriyal <namanpokhriyal03@gmail.com>
2022-04-14 01:37:24 +0530

    Grid and response

--- src/components/Screen2.js
+++ src/components/Screen2.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
+import event1 from "./Images/Cultural.png";
+import event2 from "./Images/Debate.png";
+import event3 from "./Images/Gaming.png";
+import event4 from "./Images/Technical.png";
+import event5 from "./Images/Cultural.png";
+import event6 from "./Images/Cultural.png";
 
 function Screen2() {
   return (
     <>
-    <div className="fullvh">
-
-    </div>
+      <div className="fullvh">
+        <div className="events-title">Top Events Happening</div>
+        <div className="event-grid">
+          <div className="event">
+            <img src={event1} alt="" />
+          </div>
+          <div className="event">
+            <img src={event2} alt="" />
+          </div>
+          <div className="event">
+            <img src={event3} alt="" />
+          </div>
+          <div className="event">
+            <img src={event4} alt="" />
+          </div>
+          <div className="event">
+            <img src={event5} alt="" />
+          </div>
+          <div className="event">
+            <img src={event6} alt="" />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Screen2
+export default Screen2;
